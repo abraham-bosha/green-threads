803a921195a1aa08b21eb794bc6b5501608c1891
Abraham Bosha <142293555+abraham-bosha@users.noreply.github.com>
2026-07-07 17:41:13 +0300

    feat(common): introduce mem.h

---
+++ internal/gt_internal/common/mem.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * @brief Hard-wired architecture memory constants and memory size primitives.
+ */
+
+/**
+ * @brief Standard virtual memory page size layout boundary for x86_64 architectures (4KB).
+ */
+#define GT_PAGE_SIZE 4096UL
